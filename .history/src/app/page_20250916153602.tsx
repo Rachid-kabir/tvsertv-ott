27a0f58f7fc8305d77c61b840b54a947a653784d
Rachid-kabir <52111231+Rachid-kabir@users.noreply.github.com>
2025-09-16 21:23:43 +0100

    Delete .history directory

--- .history/src/app/page_20250916153602.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-    return (
-        <div>
-            <Image src="/media/meilleur-abonnment-tv-2025.webp" alt="logo" width={0} height={0} sizes="100vw" style={{ width: "100%", height: "auto" }} />
-        </div>
-    );
-}
